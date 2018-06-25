90be1eb734747b664b475e8855614ad43a31d81d
VrushankV <iamvrushank@gmail.com>
2018-06-25 20:28:01 +0530

    Final working version

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,42 +2,133 @@ package sample;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 
 public class Main extends Application {
     private Pane pane = new Pane();
+    private String url=null;
+    private int count;
+
+    private int tries;
+    private Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+    private double height = bounds.getHeight()-50;
+    private double width = bounds.getWidth();
+    private double r;
+    private double a=0;
 
 
+    private ArrayList<Double> dps = new ArrayList<>();
     private ArrayList<Double> valArr = new ArrayList<>();
     private ArrayList<Double> info = new ArrayList<>();
     private ArrayList<Integer> sNumbers = new ArrayList<>();
     private ArrayList<Double> uniqInfo = new ArrayList<>();
+    private ArrayList<Integer> uniqsNumbers = new ArrayList<>();
     private HashMap<Integer,Double> map = new HashMap<>();
+    private Label attemptsLeft;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        Button openFile = new Button("Open a file...");
+        openFile.setLayoutX(width*0.05);
+        openFile.setLayoutY(height*0.2);
+        openFile.setFont(new Font(22));
+        pane.getChildren().add(openFile);
+
+
+        openFile.setOnAction(event -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("EXCEL FILES", "*.xlsx"));
+            File file = fileChooser.showOpenDialog(null);
+
+            String add = file.getAbsolutePath();
+            String address = add.replace("\\","\\\\");
+            getUrl(address);
+            loadData();
+            count = info.size();
+            attemptsLeft = new Label("Attempts Left = "+count);
+            attemptsLeft.setLayoutY(height*0.3);
+            attemptsLeft.setLayoutX(width*0.05);
+            attemptsLeft.setFont(new Font(22));
+            pane.getChildren().add(attemptsLeft);
+            generator();
+        });
+
+
+
+
+
+        Stage window;
+        window = primaryStage;
+        window.setTitle("Virtual Localization");
+
+
+//----------------------------------PANE INITIALISATION--------------------------------
+
+        Label head = new Label("Localization");
+        head.setFont(new Font(40));
+        head.setLayoutX(width*0.05);
+        head.setLayoutY(height*0.05);
+        pane.getChildren().add(head);
+
+
+        Button calculate = new Button("Calculate");
+        calculate.setLayoutY(height*0.4);
+        calculate.setLayoutX(width*0.05);
+        calculate.setFont(new Font(22));
+        calculate.setOnAction(this::handleButtonAction);
+        pane.getChildren().addAll(calculate);
+
+//-----------------------------------P END-------------------------------------------------
+
+        Scene scene = new Scene(pane,width,height);
+        window.setScene(scene);
+        window.resizableProperty().setValue(Boolean.FALSE);
+        window.show();
+    }
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    //------------------------------------FUNCTION DEFNS------------------------------------------
+    private void getUrl(String address){
+        url = address;
+    }
+
+    private void loadData(){
         XSSFWorkbook workbook = new XSSFWorkbook();
-        try{workbook = new XSSFWorkbook(new FileInputStream(new File("C:\\Users\\user\\IdeaProjects\\AIISH GUI new\\src\\sample\\Data.xlsx")));}
-        catch(FileNotFoundException e)
+        try{workbook = new XSSFWorkbook(new FileInputStream(new File(url)));}
+        catch(Exception e)
         {
             System.out.println("Exceptional");
 
@@ -63,94 +154,52 @@ public class Main extends Application {
 
         }
 
-        int tries = sheet.getLastRowNum() - 1;
+        tries = sheet.getLastRowNum();
+
 
         for (Double x : info){
             if(!uniqInfo.contains(x)){
                 uniqInfo.add(x);
             }
         }
-       // int n = uniqInfo.size();
 
-        for(int i=0;i<tries;i++){
-            if(!map.containsKey(sNumbers.get(i))){
-                map.put(sNumbers.get(i),uniqInfo.get(i));
+        for (Integer x: sNumbers){
+            if(!uniqsNumbers.contains(x)){
+                uniqsNumbers.add(x);
             }
-
         }
 
 
-        Stage window;
-        window = primaryStage;
-        window.setTitle("Virtual Localization");
 
+        for(int i = 0; i<tries; i++){
+            if(!map.containsKey(sNumbers.get(i))){
+                map.put(sNumbers.get(i),info.get(i));
+            }
+        }
 
-//----------------------------------GRID PANE INITIALISATION--------------------------------
-
-        Label head = new Label("Localization");
-        head.setFont(new Font(32));
-        head.setLayoutX(200);
-        head.setLayoutY(20);
-        pane.getChildren().add(head);
-
-
-
-//-----------------------------------G P END-------------------------------------------------
-
-//------------------------------------FUNCTIONS-----------------------------------------------
-        generator();
-
-
-//------------------------------------FUNCTIONS END------------------------------------------
-
-
-
-        Button calculate = new Button("Calculate");
-        calculate.setLayoutY(400);
-        calculate.setLayoutX(100);
-        calculate.setOnAction(this::handleButtonAction);
-        pane.getChildren().addAll(calculate);
-
-
-        Scene scene = new Scene(pane,1000,650);
-        window.setScene(scene);
-        window.show();
     }
 
-
-//------------------------------------FUNCTION DEFNS------------------------------------------
-
-
-//-------------------------------------FUNCTION DEFN ENDS-----------------------------------------
-    public static void main(String[] args) {
-        launch(args);
-    }
 
     private void generator(){
 
         Circle circle = new Circle();
-        circle.setCenterX(675);
-        circle.setCenterY(325);
-        circle.setRadius(300);
+        circle.setCenterX(width*0.65);
+        circle.setCenterY(height*0.5);
+        circle.setRadius(height*0.45);
         circle.setStroke(Color.BLACK);
         circle.setFill(Color.BEIGE);
 
         //-----------------------------------CIRCLE BUTTONS------------------------------------------
 
         pane.getChildren().addAll(circle);
-        generateButtons(circle.getRadius(),circle.getCenterX(),circle.getCenterY(),uniqInfo);
-
-        //-----------------------------------CIRCLE BUTTONS END--------------------------------------
+        generateButtons(circle.getRadius(),circle.getCenterX(),circle.getCenterY());
 
     }
 
-    private void generateButtons(double r, double x, double y, ArrayList<Double> selected) {
+    private void generateButtons(double r, double x, double y) {
         double targetx, targety;
         int i = 0;
-        Collections.sort(selected);
-
-        for (double sel : selected) {
-            i++;
+        for (double sel : uniqInfo) {
             if (0 <= sel && sel < 90) {
                 sel = 270 + sel;
             } else {
@@ -158,35 +207,89 @@ public class Main extends Application {
             }
             targetx = r * (Math.cos(Math.PI * sel / 180));
             targety = r * (Math.sin(Math.PI * sel / 180));
-            Button btn = new Button(Integer.toString(i));
+            Button btn = new Button(Integer.toString(uniqsNumbers.get(i)));
             btn.setLayoutX(x + targetx);
             btn.setLayoutY(y + targety);
             btn.setOnAction(this::handleButtonAction);
             pane.getChildren().add(btn);
+            i++;
         }
     }
-
+    //-----------------------------------CIRCLE BUTTONS END--------------------------------------
     private void handleButtonAction(ActionEvent event) {
+        count--;
+
         if(((Button) event.getSource()).getText().equals("Calculate")){
+            count++;
+            attemptsLeft.setText("Attempts Left: "+count);
             DOEcalc();
+            DOEperSpeaker();
+            writed();
         }
         else{
+            attemptsLeft.setText("Attempts Left: "+count);
             String s = ((Button)event.getSource()).getText();
             valArr.add(map.get(Integer.parseInt(s)));
         }
 
     }
 
-    private void DOEcalc(){
-        System.out.println(valArr.size());
-        double sum=0;
+    private void DOEperSpeaker() {
+        VBox box = new VBox();
+        box.setPadding(new Insets(5));
+        box.setLayoutX(width*0.05);
+        box.setLayoutY(height*0.6);
+        Label l = new Label("DOE PER SPEAKER:");
+        l.setFont(new Font(20));
+        box.getChildren().add(l);
 
-        for (double x : info){
-            System.out.println(x);
+        for (Integer uniqsNumber : uniqsNumbers) {
+            double sum = 0;
+            double result;
+            double z,r,s;
+            int x;
+            ArrayList<Double> tempValues = new ArrayList<>();
+            for (int j = 0; j < sNumbers.size(); j++) {
+                if (uniqsNumber.equals(sNumbers.get(j))) {
+                    tempValues.add(valArr.get(j));
+                }
+            }
+            for (int i=0;i<tempValues.size();i++) {
+                x = sNumbers.indexOf(uniqsNumber);
+                s = map.get(sNumbers.get(x));
+                r = tempValues.get(i);
+                z = r-s;
+                if(Math.abs(r-s)<=180) {
+                    sum = sum + Math.pow((z), 2);
+                }
+                else{
+
+
+                    if(r>180){
+                        r = 360-r;
+                    }
+                    else if (s>180){
+                        s=360-s;
+                    }
+                    z=r+s;
+                    sum = sum + Math.pow((z), 2);
+                }
+            }
+            result = sum / tempValues.size();
+            result = Math.sqrt(result);
+            dps.add(result);
+            a = a + result;
+            box.getChildren().add(new Label("SPEAKER "+ String.valueOf(uniqsNumber)+"=" + String.valueOf(result)));
+
         }
-        for (double x : valArr){
-            System.out.println(x);
-        }
+        a = a/uniqsNumbers.size();
+        box.getChildren().add(new Label("Average DOE per speaker = "+String.valueOf(a)));
+        pane.getChildren().add(box);
+
+    }
+
+    private void DOEcalc(){
+        double sum=0;
 
         for (int i =0;i<valArr.size();i++){
             double r=valArr.get(i);
@@ -208,16 +311,90 @@ public class Main extends Application {
                 sum = sum + Math.pow((z), 2);
             }
         }
-        System.out.println(sum);
 
-        double result = sum/info.size();
-        result = Math.sqrt(result);
-        Label res = new Label("DOE is: " + result);
+        r = sum/info.size();
+        r = Math.sqrt(r);
+        Label res = new Label("DOE is: " + r);
         res.setFont(new Font(30));
-        res.setLayoutY(600);
-        res.setLayoutX(100);
+        res.setLayoutY(height*0.9);
+        res.setLayoutX(width*0.05);
         pane.getChildren().add(res);
 
-
     }
+
+    private void writed() {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        try{workbook = new XSSFWorkbook(new FileInputStream(new File(url)));}
+        catch(Exception e)
+        {
+            System.out.println("Exceptional");
+
+        }
+
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        Row row;
+
+        int i=0;
+        row = sheet.createRow(tries+4);
+        Cell res = row.createCell(3);
+        res.setCellValue("DOE=");
+        Cell ult = row.createCell(4);
+        ult.setCellValue(r);
+
+        row = sheet.getRow(1);
+        Cell c = row.createCell(5);
+        c.setCellValue("DOE PER  SPEAKER");
+
+        row = sheet.getRow(2);
+        Cell c1 = row.createCell(5);
+        c1.setCellValue("Speaker No.");
+
+        Cell c2 = row.createCell(6);
+        c2.setCellValue("DOE");
+        for(int rowIndex =1;rowIndex<=tries;rowIndex++)
+        {
+            //------Response---------
+            row = sheet.getRow(rowIndex);
+            org.apache.poi.ss.usermodel.Cell cell=row.createCell(2);
+            cell.setCellValue(valArr.get(i));
+            //--------- R END--------
+            //-----DOE PER SPEAKER-------
+            row = sheet.getRow(rowIndex+2);
+            if(i<uniqsNumbers.size()) {
+                Cell cell1 = row.createCell(5);
+                cell1.setCellValue(uniqsNumbers.get(i));
+            }
+            if (i<dps.size()){
+                Cell cell2 = row.createCell(6);
+                cell2.setCellValue(dps.get(i));
+            }
+            //--------DPS END ------------
+            i++;
+        }
+
+        row = sheet.getRow(uniqsNumbers.size()+3);
+        Cell cx = row.createCell(5);
+        cx.setCellValue("AVG. DOE=");
+
+        cx = row.createCell(6);
+        cx.setCellValue(a);
+        FileOutputStream file = null;
+        try {
+            file = new FileOutputStream(new File(url));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            workbook.write(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            file.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+//-------------------------------------FUNCTION DEFN ENDS-----------------------------------------
+
 }
